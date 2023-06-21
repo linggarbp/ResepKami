71061b7c3faefa1159771446f1190c3d8722e4ba
Linggar Bhakti Pratama <linggarbhakti450@gmail.com>
2023-06-21 14:51:19 +0700

    Context Controller

---
+++ API/Base/GeneralController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+    }
+}
---
+++ API/Context/MyContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Context
+{
+    public class MyContext : DbContext
+    {
+    }
+}
